2ef931ed2cae645cefad1cbf6088e6c0426cbc4e
Trần Tiến Cường <tiencuongtran.0211@gmail.com>
2021-08-29 00:16:43 +0700

    Create api change user password

--- src/controller/auth/change-password.js
+++ src/controller/auth/change-password.js
@@ -1,5 +1,24 @@
-const { Res } = require('../../../common');
+const { Res, Kinds } = require('../../../common');
+const { userServices } = require('../../services');
 
-module.exports = (req, res) => {
-    Res(res).ok('changePassword');
+module.exports = async (req, res) => {
+    const { userId } = req.params;
+    const { password, newPassword } = req.body;
+
+    if (!Kinds.isObjectId(userId)) {
+        Res(res).bad('Invalid userId', { userId: 1 });
+    }
+
+    if (!Kinds.isString(newPassword) || !Kinds.isString(password)) {
+        Res(res).bad('Password or newPassword are invalid.', { password: 1, newPassword: 1 });
+    }
+
+    userServices
+        .changePassword(userId, password, newPassword)
+        .then((result) => {
+            Res(res).ok(result);
+        })
+        .catch((err) => {
+            Res(res).forbidden(err);
+        });
 };
--- src/routes/auth-router.js
+++ src/routes/auth-router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const logger = require('../config/winston');
+const verifyToken = require('../middleware/verify-token');
 
 module.exports = router;
 
 router.post('/register', require('../controller/auth/register'));
 router.post('/login', require('../controller/auth/login'));
 router.post('/refresh-token', require('../controller/auth/refesh-token'));
-router.put('/change-password', require('../controller/auth/change-password'));
+router.put('/:userId/change-password', verifyToken, require('../controller/auth/change-password'));
